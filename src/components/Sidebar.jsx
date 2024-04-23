7f7f325fe3305ff5f0e4af1d6ddc17d0999227b0
zainulhasanawan <zainulhassanawan001@gmail.com>
2024-04-23 19:16:28 +0500

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import Footer from "./Footer";
+import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
